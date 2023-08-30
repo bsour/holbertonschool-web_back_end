e91b12cbcaf930a4e1524ded87ddb545d3daedde
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-30 14:52:56 +1000

    Update 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -7,6 +7,7 @@ from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
     Executes task_wait_random n times asynchronously and returns
@@ -19,5 +20,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
     - A list of float values representing the delays.
     '''
-    delay_list = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
+    delay_list = await asyncio.gather(*(task_wait_random(max_delay)
+                                        for _ in range(n)))
     return sorted(delay_list)
