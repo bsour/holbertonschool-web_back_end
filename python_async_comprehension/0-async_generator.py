3da7631ce2970e856bdae7bb8051420efe6e34bd
root <sourabhbeniwal@outlook.com>
2023-08-31 02:06:11 +0000

    added initital files and task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,14 +4,15 @@ a coroutine called async_generator that takes no arguments
 '''
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     '''
     print i in a loop 10 times
     with random number from 0 to 10
     '''
-    for i in range(10):
-        random_num = random.randint(0, 10)
+    for _ in range(10):
+        random_num = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield i
+        yield random_num
