55ae52673c2b36f14b6c2c98090258eafb21782e
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-08-31 22:44:39 +1000

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -11,6 +11,7 @@ from importlib import import_module
 # Import async_generator using import_module
 async_generator = import_module("0-async_generator").async_generator
 
+
 async def async_comprehension() -> List[float]:
     '''
     collects 10 random number using async comp
