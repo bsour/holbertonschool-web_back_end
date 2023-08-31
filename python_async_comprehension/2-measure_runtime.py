0f1ac611a7f876203c9433c130b35dc421848a90
root <sourabhbeniwal@outlook.com>
2023-08-31 12:53:48 +0000

    added task 3

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+measuring runtime for parallel comprehensions
+'''
+import asyncio
+from typing import List
+from time import perf_counter
+
+from 1-async_comprehension import async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    Measures the total runtime for running
+    async_comprehension four times in parallel.
+    '''
+    start_time = perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = perf_counter()
+    total_runtime = end_time - start_time
+    return total_runtime
