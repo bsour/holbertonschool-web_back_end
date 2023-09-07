fb164b4462e7e0085ca8a55e6ef34716ceacf075
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-09-07 15:41:37 +1000

    Update 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+'''
+Implement a get_hyper method that takes the same
+arguments (and defaults) as get_page and returns
+a dictionary containing the following key-value pairs:
+'''
 import csv
 import math
 from typing import List
