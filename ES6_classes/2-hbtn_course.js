bc819a776d8e89605e710ca153f4e6ee5556e52a
Sourabh <sourabhbeniwal@outlook.com>
2023-09-16 13:14:46 +0000

    error fix

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,49 +1,44 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    // Verify the types of attributes during object creation
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw a TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
-  // Getter for name
   get name() {
     return this._name;
   }
 
-  // Setter for name
-  set name(newName) {
-    this._name = newName;
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
 
-  // Getter for length
   get length() {
     return this._length;
   }
 
-  // Setter for length
-  set length(newLength) {
-    this._length = newLength;
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 
-  // Getter for students
   get students() {
     return this._students;
   }
 
-  // Setter for students
-  set students(newStudents) {
-    this._students = newStudents;
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
 
