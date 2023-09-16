35d9ca5566c2ebec51bc380c5b5993ef3c59ea2a
Sourabh <sourabhbeniwal@outlook.com>
2023-09-16 13:31:03 +0000

    added task 3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newcode) {
+    this._code = newcode;
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set name(newname) {
+    this._name = newname;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
