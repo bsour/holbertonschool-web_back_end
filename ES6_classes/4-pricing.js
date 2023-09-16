e8a5387bcc11c308c141ba556fa21869a091393b
Sourabh <sourabhbeniwal@outlook.com>
2023-09-16 13:54:07 +0000

    added task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from "./3-currency";
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newamount) {
+    this._amount = newamount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newcurrency) {
+    this._currency = newcurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`; // Use getters for name and code
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    } else {
+      throw new TypeError('Invalid parameters');
+    }
+  }
+}
+
+export default Pricing;
