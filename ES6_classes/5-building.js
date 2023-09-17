87a7613f1c3e457a78fdcaaf2e29efb8c6ec835a
Sourabh <sourabhbeniwal@outlook.com>
2023-09-17 03:53:15 +0000

    try again

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,15 @@
-export default class Building {
+class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building) {
-      this.evacuationWarningMessage();
-    }
-  }
-
-  evacuationWarningMessage() {
-    this._sqrt = this._sqft;
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
   get sqft() {
     return this._sqft;
   }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
+
+export default Building;
