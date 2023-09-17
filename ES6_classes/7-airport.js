038b9e6fe01f47a0f0ab39401b90fcf0b32e3ade
Sourabh <sourabhbeniwal@outlook.com>
2023-09-17 04:36:57 +0000

    another attempt

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,11 +1,7 @@
 class Airport {
   constructor(name, code) {
-    if (typeof name === 'string' && typeof code === 'string') {
-      this._name = name;
-      this._code = code;
-    } else {
-      throw Error('invalid input');
-    }
+    this._name = name;
+    this._code = code;
   }
 
   toString() {
