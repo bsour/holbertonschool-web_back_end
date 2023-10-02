2b116a1bcd533b545a4a5ffba20e1157db4f4b24
Sourabh <sourabhbeniwal@outlook.com>
2023-10-02 23:47:12 +1100

    task 3 error fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch((error) => {
-      console.error('Signup system offline', error);
-    });
+async function handleProfileSignup() {
+  try {
+    // Use Promise.all to collectively resolve all promises
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    // Log body firstName and lastName to the console
+    console.log(`${photo.body} ${user.firstName.lastName}`);
+  } catch (error) {
+    // Handle errors
+    console.error('Signup system offline');
+  }
 }
+
+export default handleProfileSignup;
