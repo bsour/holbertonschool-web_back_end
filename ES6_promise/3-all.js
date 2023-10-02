2a13955728042688b9c84b33b848af53cdf5cd08
Sourabh <sourabhbeniwal@outlook.com>
2023-10-03 00:20:46 +1100

    task 3 correct implimentation

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,16 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-async function handleProfileSignup() {
-  try {
-    // Use Promise.all to collectively resolve all promises
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-
-    // Log body firstName and lastName to the console
-    console.log(`${photo.body} ${user.firstName.lastName}`);
-  } catch (error) {
-    // Handle errors
-    console.error('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
-
-export default handleProfileSignup;
